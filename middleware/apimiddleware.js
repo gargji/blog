afe0382c03eff7eb1b50a84701655c458987bf7e
gargji <gargjirahul@gmail.com>
2023-02-19 16:03:03 +0530

    .

--- middleware/apimiddleware.js
+++ middleware/apimiddleware.js
@@ -3,16 +3,12 @@ const UserModel= require('../models/User')
 
 
 const apimiddleware = async(req,res,next)=>{
-    console.log(req.body)
-<<<<<<< HEAD
-    // res.send(req.body)
-=======
+    // console.log(req.body)
 
->>>>>>> 5f28a3eaf8392a9195ab53dc3e564fe219f19f00
-console.log(req.body.token)
+// console.log(req.body.token)
 
 const token =req.body.token;
-console.log(token)
+// console.log(token)
 if(!token){
     res.send({ status: "failed", message: "😓" });
 
@@ -20,15 +16,15 @@ if(!token){
     const verify_token =jwt.verify(token,'vishal12345')
 //    console.log(verify_token)
 const data=await UserModel.findOne({_id:verify_token.userId})
-console.log(data)
-<<<<<<< HEAD
+// console.log(data)
+
 res.status(200).json({
     success: true,
     data:data
 })
-=======
+
 res.send({ status: "success",data:data });
->>>>>>> 5f28a3eaf8392a9195ab53dc3e564fe219f19f00
+
 
 }
 // console.log(token)
